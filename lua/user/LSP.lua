588dc61ad0324bd44119aa17f380fd2d40201a68
zDyant <dyant@proton.me>
2024-05-22 19:09:07 -0300

    Update: LSP

--- lua/user/LSP.lua
+++ lua/user/LSP.lua
@@ -1,20 +1,18 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
   automatic_installation = false,
-  ensure_installed = {
-    "lua_ls",
-    "rust_analyzer",
-    "bashls",
-    "marksman",
-    "clangd",
-    "cssls",
-    "jsonls",
-    "lua_ls"
-  },
+  -- ensure_installed = {
+  --   "bashls",
+  --   "marksman",
+  --   "clangd",
+  --   "cssls",
+  --   "jsonls",
+  --   "lua_ls"
+  -- },
 }
 
+-- Do not call the nvim-lspconfig.rust_analyzer setup or set up the LSP client for rust-analyzer manually, as doing so may cause conflicts.
 require("lspconfig").lua_ls.setup {}
-require("lspconfig").rust_analyzer.setup {}
 require("lspconfig").marksman.setup {}
 require("lspconfig").bashls.setup {}
 require("lspconfig").clangd.setup {}
